dec8f8116589bd7da24328dcdeb64d985422c2f6
MarinaHarmash <email@gmail.com>
2022-04-25 19:02:21 +0300

    add the first exercise

--- Udemy_course/python.py
+++ Udemy_course/python.py
@@ -1,3 +1,40 @@
-name = input("Enter your name: ")
-if name == "Victor":
-    print("There is ")
+# name = input("Enter your name: ")
+# if name == "Victor":
+#     print("Such user already exists")
+# elif name == "Denis":
+#     print("You are an autor of course")
+# else:
+#     print("ok")
+
+# a = int(input())
+
+# if a > 10:
+#     print("More than 10")
+# else:
+#     print("Less or equal 10")
+
+# if a >10 and a < 20:
+#     print("Interval is correct")
+# else:
+#     print("Other")
+
+# if a > 30 or a < 20:
+#     print("Ok")
+# else:
+#     print("Other")
+
+# if not a > 5:
+#     print("Less than 5")
+# else:
+#     print("More than 5")
+                                            # exercice 1 "if,elif,else"
+# a = int(input("Рекомендации по сну: "))
+# b = int(input("Нужно спать не более: "))
+# c = int(input("Введите количество чвсов вашего сна: "))
+# if c == a:
+#     print("У вас все хорошо со сном")
+# elif c < a:
+#     print("У вас недосып!")
+# else:
+#     print("У вас пересып")
+
