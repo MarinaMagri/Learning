d89210f57946109d0a7b460b03394523ca2f6e08
MarinaHarmash <email@gmail.com>
2022-05-04 09:54:07 +0300

    commit

--- Udemy_course/GUI/mainGUI.py
+++ Udemy_course/GUI/mainGUI.py
@@ -1,34 +1,54 @@
+# from tkinter import *
+
+
+# root = Tk()
+# root.title("Application")
+# root.geometry("640x480")
+# root.resizable(width = False, height = False)
+
+
+# cnt = 0 
+
+# def Clicked():
+#     global cnt
+#     cnt +=1
+#     print(f"{cnt} clicks")
+
+# button = Button(
+#     text = "button",
+#     width = 20,
+#     height = 10,
+#     background = "yellow",
+#     foreground = "red",
+#     font = ("Verdana", "24"),
+#     command = Clicked
+# )
+# button.pack()
+
+# button2 = Button(
+#     text = "button 2"
+# )
+# button2.pack()
+
+# if __name__ == "__main__":
+#     root.mainloop()
+
+
 from tkinter import *
 
 
 root = Tk()
-root.title("Application")
+root.title("Aplication")
 root.geometry("640x480")
 root.resizable(width = False, height = False)
 
 
-cnt = 0 
-
-def Clicked():
-    global cnt
-    cnt +=1
-    print(f"{cnt} clicks")
-
 button = Button(
-    text = "button",
-    width = 20,
-    height = 10,
-    background = "yellow",
-    foreground = "red",
-    font = ("Verdana", "24"),
-    command = Clicked
+text = "123"
 )
 button.pack()
 
-button2 = Button(
-    text = "button 2"
-)
-button2.pack()
 
-if __name__ == "__main__":
-    root.mainloop()
+
+if __name__ == "__main__" :
+    root.mainloop
