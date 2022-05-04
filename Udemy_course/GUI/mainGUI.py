47bc8f376003f77a7a22ac115f66374dc21a8a8d
MarinaHarmash <email@gmail.com>
2022-05-04 09:40:13 +0300

    add the first button

---
+++ Udemy_course/GUI/mainGUI.py
@@ -0,0 +1,34 @@
+from tkinter import *
+
+
+root = Tk()
+root.title("Application")
+root.geometry("640x480")
+root.resizable(width = False, height = False)
+
+
+cnt = 0 
+
+def Clicked():
+    global cnt
+    cnt +=1
+    print(f"{cnt} clicks")
+
+button = Button(
+    text = "button",
+    width = 20,
+    height = 10,
+    background = "yellow",
+    foreground = "red",
+    font = ("Verdana", "24"),
+    command = Clicked
+)
+button.pack()
+
+button2 = Button(
+    text = "button 2"
+)
+button2.pack()
+
+if __name__ == "__main__":
+    root.mainloop()
