90b95c03f532ddeb646ac82775273ec04e102440
MarinaHarmash <email@gmail.com>
2022-05-04 10:12:18 +0300

    add the button

--- Udemy_course/GUI/mainGUI.py
+++ Udemy_course/GUI/mainGUI.py
@@ -1,54 +1,35 @@
-# from tkinter import *
-
-
-# root = Tk()
-# root.title("Application")
-# root.geometry("640x480")
-# root.resizable(width = False, height = False)
-
-
-# cnt = 0 
-
-# def Clicked():
-#     global cnt
-#     cnt +=1
-#     print(f"{cnt} clicks")
-
-# button = Button(
-#     text = "button",
-#     width = 20,
-#     height = 10,
-#     background = "yellow",
-#     foreground = "red",
-#     font = ("Verdana", "24"),
-#     command = Clicked
-# )
-# button.pack()
-
-# button2 = Button(
-#     text = "button 2"
-# )
-# button2.pack()
-
-# if __name__ == "__main__":
-#     root.mainloop()
-
-
 from tkinter import *
 
 
 root = Tk()
-root.title("Aplication")
+root.title("Application")
 root.geometry("640x480")
 root.resizable(width = False, height = False)
 
 
+cnt = 0 
+
+def Clicked():
+    global cnt
+    cnt +=1
+    print(f"{cnt} clicks")
+
 button = Button(
-text = "123"
+    text = "button",
+    width = 20,
+    height = 10,
+    background = "yellow",
+    foreground = "red",
+    font = ("Verdana", "24"),
+    command = Clicked
 )
 button.pack()
 
+button2 = Button(
+    text = "button 2"
+)
+button2.pack()
 
-
-if __name__ == "__main__" :
-    root.mainloop
+if __name__ == "__main__":
+    root.mainloop()
+# 
